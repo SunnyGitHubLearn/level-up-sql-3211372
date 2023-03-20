27b0ca8f2d938f14053b104a91cd56c5b8d54b99
SunnyGitHubLearn <126856287+SunnyGitHubLearn@users.noreply.github.com>
2023-03-20 06:29:29 +0000

    SQL Answers Updated

--- 01_restaurant/challenges/01_03_printmenus_ch.sql
+++ 01_restaurant/challenges/01_03_printmenus_ch.sql
@@ -3,3 +3,7 @@
 -- Create a report with all the items sorted by price (lowest to highest).
 -- Create a report showing appetizers and beverages.
 -- Create a report with all items except beverages.
+
+SELECT * From Dishes Order By Price;
+Select * From Dishes Where Type in ('Appetizer','Beverage') Order By Type;
+Select * From Dishes Where Type not in ('Beverage') Order By Type;
