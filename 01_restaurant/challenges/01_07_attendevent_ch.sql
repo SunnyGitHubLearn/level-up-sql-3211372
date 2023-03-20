0c55552a6b09900bc881cdf874f46f740bcaa596
SunnyGitHubLearn <126856287+SunnyGitHubLearn@users.noreply.github.com>
2023-03-20 07:39:38 +0000

    Update SQL Answer Query

--- 01_restaurant/challenges/01_07_attendevent_ch.sql
+++ 01_restaurant/challenges/01_07_attendevent_ch.sql
@@ -2,3 +2,11 @@
 
 -- The customer 'atapley2j@kinetecoinc.com' will be in
 -- attendance, and will bring 3 friends.
+
+Select * From Customers Where Email = 'atapley2j@kinetecoinc.com'
+
+Insert Into AnniversaryAttendees
+Select CustomerID,3
+From Customers Where Email = 'atapley2j@kinetecoinc.com'
+
+Select * From AnniversaryAttendees
