7430b1d40ccc1e73403718a46ba15e7ef3027c35
SunnyGitHubLearn <126856287+SunnyGitHubLearn@users.noreply.github.com>
2023-03-20 07:30:03 +0000

    modified:   01_restaurant/challenges/01_06_removeinfo_ch.sql modified:   01_restaurant/restaurant.db
    SQL Query Updated

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,38 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+
+Select r.* 
+From Reservations r, Customers c
+Where r.CustomerID = c.CustomerID
+And c.FirstName = 'Norby'
+And Date('2022-07-24','weekday 5') = Date(Date);
+
+--Update Reservations r
+--Set Date = Null
+--From Reservations r, Customers c
+--Where r.CustomerID = c.CustomerID
+--And c.FirstName = 'Norby'
+--And Date('2022-07-24','weekday 5') = Date(r.Date);
+
+Select *
+From Reservations
+Where ReservationID =  (Select r.ReservationID From Reservations r, Customers c
+                        Where r.CustomerID = c.CustomerID
+                        And c.FirstName = 'Norby'
+                        And Date('2022-07-24','weekday 5') = Date(r.Date)
+                        );
+
+
+Update Reservations
+Set Date = Null
+Where ReservationID =  (Select r.ReservationID From Reservations r, Customers c
+                        Where r.CustomerID = c.CustomerID
+                        And c.FirstName = 'Norby'
+                        And Date('2022-07-24','weekday 5') = Date(r.Date)
+                        );
+
+
+
+--Select strftime('%w','2022-07-24')--, date('2022-7-24',)
+--Select Date('2022-07-24','weekday 5')
