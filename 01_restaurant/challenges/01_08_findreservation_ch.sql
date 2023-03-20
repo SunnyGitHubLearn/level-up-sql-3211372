4bec0ea722312850ea4ed7a20a5be746a4f3049b
SunnyGitHubLearn <126856287+SunnyGitHubLearn@users.noreply.github.com>
2023-03-20 07:55:11 +0000

    SQL Query Updated

--- 01_restaurant/challenges/01_08_findreservation_ch.sql
+++ 01_restaurant/challenges/01_08_findreservation_ch.sql
@@ -5,3 +5,14 @@
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
 
 -- There are four people in the party. Today is June 14th.
+
+Select * From Reservations
+
+SELECT *
+From Reservations r, Customers c
+Where r.CustomerID = c.CustomerID
+And r.PartySize = 4
+And c.LastName Like 'Ste%'
+And  strftime('%m-%d',r.Date) = strftime('%m-%d','2023-06-14')
+
+--Select strftime('%m-%d','2018-07-14')
