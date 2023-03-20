e1427c80a112ba5784c311575923e1a02cdd6710
SunnyGitHubLearn <126856287+SunnyGitHubLearn@users.noreply.github.com>
2023-03-20 08:05:01 +0000

    SQL Queries Updated

--- 01_restaurant/challenges/01_09_createreservation_ch.sql
+++ 01_restaurant/challenges/01_09_createreservation_ch.sql
@@ -4,3 +4,19 @@
 -- Use the following information:
 -- Sam McAdams (smac@kinetecoinc.com), for 5 people
 -- on August 12, 2022 at 6PM (18:00)
+
+Select * From Customers 
+Where FirstName = 'Sam' And LastName = 'McAdams' 
+
+Insert Into Customers(FirstName,LastName,Email)
+Select 'Sam','McAdams' ,'smac@kinetecoinc.com'
+
+Insert Into Reservations (CustomerID,PartySize,Date)
+Select CustomerID,5,'2022-08-12 18:00:00'
+From Customers
+Where FirstName = 'Sam' And LastName = 'McAdams' 
+
+Select r.*
+From Reservations r, Customers c
+Where r.CustomerID = c.CustomerID
+And FirstName = 'Sam' And LastName = 'McAdams' 
