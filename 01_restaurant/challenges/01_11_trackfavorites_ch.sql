ab30f4f51032ccdefad0b3b464b43e3618596d8b
SunnyGitHubLearn <126856287+SunnyGitHubLearn@users.noreply.github.com>
2023-03-28 09:20:55 +0000

    SQL Query updated

--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -2,3 +2,14 @@
 
 -- Set Cleo Goldwater's favorite dish to
 -- the Quinoa Salmon Salad.
+
+Select *
+From Customers c, Dishes d 
+Where c.FavoriteDish = d.DishID
+And FirstName = 'Cleo' And LastName = 'Goldwater'
+
+(Select DishID From Dishes Where Name = 'Quinoa Salmon Salad')
+
+Update Customers
+Set FavoriteDish = (Select DishID From Dishes Where Name = 'Quinoa Salmon Salad')
+Where FirstName = 'Cleo' And LastName = 'Goldwater'
