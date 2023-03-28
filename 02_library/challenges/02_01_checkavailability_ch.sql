262fa7a58a26d51193c0a2b56099777aa9511e64
SunnyGitHubLearn <126856287+SunnyGitHubLearn@users.noreply.github.com>
2023-03-28 09:47:56 +0000

    SQL Query updated

--- 02_library/challenges/02_01_checkavailability_ch.sql
+++ 02_library/challenges/02_01_checkavailability_ch.sql
@@ -1,2 +1,11 @@
 -- Determine how many copies of the book 'Dracula'
 -- are available for library patrons to borrow.
+
+Select Count(*) From Books Where Title = 'Dracula'
+And BookID not in (
+SELECT l.BookID
+From Loans l, Books B
+Where l.BookID = b.BookID
+And b.Title = 'Dracula'
+And ReturnedDate is Null
+Group By l.BookID)
