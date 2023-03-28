d7bc2e7fcad71875eb8e8232849daa0580e3b071
SunnyGitHubLearn <126856287+SunnyGitHubLearn@users.noreply.github.com>
2023-03-28 09:54:18 +0000

    SQL Query Updated

--- 02_library/challenges/02_02_addbook_ch.sql
+++ 02_library/challenges/02_02_addbook_ch.sql
@@ -9,3 +9,11 @@
 -- Author: Jonathan Swift
 -- Published: 1729
 -- ID Number: 4899254401
+
+
+Insert Into Books (Title,Author,Published,Barcode)
+Values
+('Dracula','Bram Stoker',1897,4819277482),
+("Gulliver's Travels into Several Remote Nations of the World",'Jonathan Swift',1729,4899254401)
+
+Select * from Books Order By BookID Desc
