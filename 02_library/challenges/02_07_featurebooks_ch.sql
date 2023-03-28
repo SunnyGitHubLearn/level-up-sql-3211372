d9a65a83c3f352baff00c878007b6df4460ae365
SunnyGitHubLearn <126856287+SunnyGitHubLearn@users.noreply.github.com>
2023-03-28 10:32:02 +0000

    SQL Query updated

--- 02_library/challenges/02_07_featurebooks_ch.sql
+++ 02_library/challenges/02_07_featurebooks_ch.sql
@@ -2,3 +2,8 @@
 
 -- Make a pick list of books published from 1890-1899 
 -- which are not currently checked out.
+
+Select *
+From Books
+Where Published between 1890 and 1899
+And BookID not in (Select BookID from Loans Where ReturnedDate is Null)
