ee204e3315e7eedcc5dba68de3593da0941a970d
SunnyGitHubLearn <126856287+SunnyGitHubLearn@users.noreply.github.com>
2023-03-28 10:02:24 +0000

    SQL Query updated

--- 02_library/challenges/02_03_checkout_ch.sql
+++ 02_library/challenges/02_03_checkout_ch.sql
@@ -3,3 +3,11 @@
 -- Book 2: Great Expectations, 4043822646
 -- The checkout date is August 25, 2022 and the 
 -- due date is September 8, 2022.
+
+Select * From Loans Order By LoanID Desc Limit 5
+
+Insert Into Loans(BookID,PatronID,LoanDate,DueDate,ReturnedDate)
+SELECT b.BookID,p.PatronID,'2022-08-25','2022-09-08','2022-09-08'
+From Books b, Patrons p
+Where b.Barcode in (2855934983,4043822646)
+And p.Email = 'jvaan@wisdompets.com'
