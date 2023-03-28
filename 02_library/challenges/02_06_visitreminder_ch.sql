2d5ca97a0435c8cc33ff908696c72e02641f3f3a
SunnyGitHubLearn <126856287+SunnyGitHubLearn@users.noreply.github.com>
2023-03-28 10:24:45 +0000

    SQL Query updated

--- 02_library/challenges/02_06_visitreminder_ch.sql
+++ 02_library/challenges/02_06_visitreminder_ch.sql
@@ -1,2 +1,14 @@
 -- Prepare a report of the library patrons
 -- who have checked out the fewest books.
+
+Select c.CheckedOutCount,p.*
+From Patrons p,
+(
+SELECT PatronID, Count(*) 'CheckedOutCount'
+From Loans
+Where ReturnedDate is Not NULL
+Group BY PatronID
+)c
+Where p.PatronID = c.PatronID
+Order By 1 
+Limit 11
