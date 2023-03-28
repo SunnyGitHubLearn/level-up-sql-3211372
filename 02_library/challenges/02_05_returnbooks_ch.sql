b7fdfb76d4bd590b73219945f04237241d8b0af7
SunnyGitHubLearn <126856287+SunnyGitHubLearn@users.noreply.github.com>
2023-03-28 10:17:57 +0000

    SQL Query updated

--- 02_library/challenges/02_05_returnbooks_ch.sql
+++ 02_library/challenges/02_05_returnbooks_ch.sql
@@ -5,4 +5,16 @@
 -- Book 2: 5677520613
 -- Book 3: 8730298424
 
-Select
+Select LoanID,ReturnedDate
+From Books b, Loans l
+Where b.BookID = l.BookID
+And b.barcode in (6435968624,5677520613,8730298424)
+And l.ReturnedDate is Null
+
+Update Loans
+Set ReturnedDate = '2022-07-05'
+Where LoanID in (Select LoanID
+From Books b, Loans l
+Where b.BookID = l.BookID
+And b.barcode in (6435968624,5677520613,8730298424)
+And l.ReturnedDate is Null)
