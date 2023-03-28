7a7e3a27daf4efba6bb9b8fc7b5cfbaa725794ed
SunnyGitHubLearn <126856287+SunnyGitHubLearn@users.noreply.github.com>
2023-03-28 10:08:02 +0000

    SQL Query Updated

--- 02_library/challenges/02_04_booksdue_ch.sql
+++ 02_library/challenges/02_04_booksdue_ch.sql
@@ -2,3 +2,9 @@
 -- to the library on July 13, 2022.
 -- Provide the due date, the book title, and
 -- the borrower's first name and email address.
+
+SELECT l.DueDate,b.Title,p.FirstName,p.Email
+From Loans l, Books b, Patrons p
+Where l.BookID = b.BookID
+And l.PatronID = p.PatronID
+And l.DueDate = '2022-07-13'
