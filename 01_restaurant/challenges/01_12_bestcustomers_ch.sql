0276b58a46c7ba6269fb8a883d45212e5f6352fe
SunnyGitHubLearn <126856287+SunnyGitHubLearn@users.noreply.github.com>
2023-03-28 09:30:31 +0000

    SQL Query Updated

--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,3 +1,10 @@
 -- Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
+
+Select Max(c.FirstName) FirstName,Max(c.LastName) LastName,Max(c.email) email,Count(*) NoOfOrders
+From Orders o,Customers c
+Where o.CustomerID = c.CustomerID
+Group By o.CustomerID
+Order By NoOfOrders Desc
+Limit 15;
